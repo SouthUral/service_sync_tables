3e940544df8623f7bab56aec07dddd0892fea801
vladimir <vladimir1012kov@gmail.com>
2023-09-01 11:10:54 +0300

    переделываю структуру взаимодействия state и postgres

--- database/postgres/models.go
+++ database/postgres/models.go
@@ -9,3 +9,14 @@ type IncomingMess struct {
 
 // Канал для входящих сообщений
 type IncomCh chan IncomingMess
+
+// Структура для передачи сообщений из горутины с синхронизацией в горутину состояния.
+type OutgoingMessSync struct {
+	Info   string
+	Offset string
+	Error  error
+	id     string
+}
+
+// канал для исходящих сообщений от горутин синхронизаций для модуля state
+type OutgoingChanSync chan OutgoingMessSync
--- database/postgres/postgres.go
+++ database/postgres/postgres.go
@@ -23,15 +23,42 @@ func pgConnect(dbURL string) interface{} {
 
 // Структура для хранения каналов, которые переданы при инициализации в main
 type postgresMain struct {
-	urlIncomCh url.InputUrlStorageAPIch
-	pgInputch  IncomCh
-	mainDB     string // alias БД из которой будет производится выгрузка
+	OutgoingChan OutgoingChanSync         // канал для сообщений от горутин синхронизаций
+	urlIncomCh   url.InputUrlStorageAPIch // канал для запросов к модулю urlstorage
+	pgInputch    IncomCh                  // канал для получения сообщений от других модулей
+	mainDB       string                   // alias БД из которой будет производится выгрузка
 }
 
 func InitPostgres(urlIncomCh url.InputUrlStorageAPIch, pgInputch IncomCh) {
 	pg := postgresMain{
 		urlIncomCh: urlIncomCh,
 		pgInputch:  pgInputch,
+		mainDB:     "main_db",
 	}
+	// Запуск основного потока postgres
+	go pg.mainWorkPg()
 	log.Debug(fmt.Sprintf("Создана структура %T", pg))
+}
+
+func (pg *postgresMain) mainWorkPg() {
+	for {
+		select {
+		case mess := <-pg.pgInputch:
+			mainUrlConn, err := url.GetOneConnURL(pg.mainDB, pg.urlIncomCh)
+			if err != nil {
+				log.Error(fmt.Sprintf("Не найден URL для mainDB: %s", err.Error()))
+				return
+			}
+			urlConn, err := url.GetOneConnURL(mess.Database, pg.urlIncomCh)
+			if err != nil {
+				log.Error(err.Error())
+				return
+			}
+			go mainStreamSync(mainUrlConn, urlConn)
+		}
+	}
+}
+
+func mainStreamSync(mainUrl, secondUrl string) {
+
 }
